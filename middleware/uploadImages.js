6b8293ecd9faf8b3fe188d1f23ac4061f4970495
Sgenne <simon.genne@gmail.com>
2021-11-21 21:08:31 +0100

    handles images uploaded without category

--- middleware/uploadImages.js
+++ middleware/uploadImages.js
@@ -3,35 +3,38 @@ const express = require("express");
 
 const Image = require("../models/image");
 
-
 const _storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "images/"),
-  filename: (req, file, cb) => cb(null, Math.random().toFixed(4) + file.originalname),
+  filename: (req, file, cb) =>
+    cb(null, Math.random().toFixed(4) + file.originalname),
 });
 
 const _fileFilter = (req, file, cb) => {
   if (file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
-    console.log("filter accept")
     cb(null, true);
   } else {
-    console.log("filter fail")
     cb(null, false);
   }
 };
 
-const _uploadImage = multer({ storage: _storage, fileFilter: _fileFilter }).single(
-  "image"
-);
+const _uploadImage = multer({
+  storage: _storage,
+  fileFilter: _fileFilter,
+}).single("image");
 
 const _updateDB = async (req, res, next) => {
-  console.log("req.body in _updateDB", req.body)
- try {
+  if (!req.body.category) {
+    const error = new Error("No image category was given."); // should also delete image
+    error.statusCode = 400;
+    return next(error);
+  }
   if (!req.file) {
-    const error = new Error("No file was uploaded.")
-    throw error;
+    const error = new Error("No file was uploaded.");
+    error.statusCode = 400;
+    return next(error);
   }
 
-  const title = req.file.filename; 
+  const title = req.file.filename;
   const imageUrl = `localhost:8080/bilder/${req.file.filename}`;
   const description = req.body.description;
   const category = req.body.category;
@@ -41,20 +44,23 @@ const _updateDB = async (req, res, next) => {
     imageUrl: imageUrl,
     description: description,
     category: category,
-  })
+  });
 
-  await image.save();
-  res.json(JSON.stringify({
-    message: "success!",
-    image: image
-  }))
- } catch(error) {
-   console.log("error in _updateDB: ", error);
-    res.status(400).json({
-      message: "Error uploading image"
+  try {
+    await image.save();
+  } catch (err) {
+    const error = new Error("Error while updating database.");
+    error.statusCode = 500;
+    return next(error);
+  }
+  
+  res.json(
+    JSON.stringify({
+      message: "Image uploaded successfully!",
+      image: image,
     })
- }
-}
+  );
+};
 
 const router = express.Router();
 
